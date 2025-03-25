c94b8e8f64e8db12a6fda0bc9636410e4d34ea9e
Suprita13 <suprita@digi9.co.in>
2025-03-25 16:12:10 +0530

    orders backend done

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const orderController = require("../controller/orderController");
 
-router.post("/createOrder", orderController.createOrder);
-router.get("/getAllOrders", orderController.getAllOrder);
+// router.post("/createOrder", orderController.createOrder);
+router.post("/createOrder", orderController.createOrder)
 router.get("/getOrder/:id", orderController.getOrderById);
+router.get("/getAllOrders", orderController.getAllOrder);
+router.get("/getOrdersByVendor/:vendor_id", orderController.getOrdersByVendor);
 router.put("/updateOrder/:id", orderController.updateOrder);
-router.delete("/deleteOrder/:id", orderController.deleteOrder);
+// router.delete("/deleteOrder/:id", orderController.deleteOrder);
 
 module.exports = router;
